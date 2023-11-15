977dc6cc25188d445601b0e9efcc23ce9eea2a50
CristianUrsino <ursino.cristian.uc@gmail.com>
2023-11-15 16:54:40 +0100

    initial commit                   (MILESTON 0)

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+</template>
+
+<script>
+  import axios from 'axios';
+  import {store} from './data/store.js'
+  export default{
+    data(){
+      return {
+        store,
+      }
+    },
+    created(){
+      // const url = this.store.apiUrl + this.store.endPoint.movies + '?api_key=' + this.store.api_key + '&query=' + this.store.query;
+
+      // axios.get(url).then((res)=>{
+      //   console.log(res.data);
+      // })
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
---
+++ src/data/store.js
@@ -0,0 +1,15 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+    apiUrl: 'https://api.themoviedb.org/3/',
+    endPoint:{
+        movies:'search/movie',
+        series:'search/tv',
+    },
+    api_key: '9aa133434562a109367588268257ad2f',
+    query:'a',
+    error: '',
+    loading:true,
+    movieList:[],
+    seriesList: [],
+});
