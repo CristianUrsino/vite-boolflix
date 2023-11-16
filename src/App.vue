cec08a1151f4ff98dd1855ed82bafcce49301813
CristianUrsino <ursino.cristian.uc@gmail.com>
2023-11-16 12:36:22 +0100

    MILESTONE 1 COMPLETE

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,28 @@
       </div>
     </header>
 
-    <main>
-      <div class="container py-3 px-1">
+    <main class="container-fluid overflow-y-auto py-3 px-1">
+      <div class="container">
         <h3 v-if="store.moviesList.length === 0 && store.seriesList.length === 0">Cercare i film/serie tv</h3>
+      </div>
+
+      <div class="container row mx-auto gy-2 debug mb-5 pb-5">
+        <div class="col-2" v-for="movies in this.store.moviesList" :key="movies.id">
+          <CardComponent
+            :title="movies.title"
+            :originalTitle="movies.original_title"
+            :originalLinguage="movies.original_language"
+            :voteAverage="movies.vote_average"
+          />
+        </div>
+        <div class="col-2" v-for="series in this.store.seriesList" :key="series.id">
+          <CardComponent
+            :title="series.name"
+            :originalTitle="series.original_name"
+            :originalLinguage="series.original_language"
+            :voteAverage="series.vote_average"
+          />
+        </div>
       </div>
     </main>
 
@@ -24,11 +43,13 @@
   import axios from 'axios';
   import {store} from './data/store.js';
   import SearchComponent from './components/SearchComponent.vue';
+  import CardComponent from './components/CardComponent.vue';
 
   export default{
     name: 'App',
     components:{
       SearchComponent,
+      CardComponent,
     },
 
     data(){
@@ -45,6 +66,7 @@
        * Prende la nuova query dal componente SearchComponent attraverso $emit, richiama il set delle liste dei parametri e richiama il getMovies e getSeries 
        * @param {String} newQuery
        * @returns {Void}
+       * !!!!!!!DIVIDERE IN 2 FUNZIONI O CAMBIARE NOME!!!!!!!!!
        */
       setQuery(newQuery){
         console.log('App ok-' + newQuery);
@@ -84,12 +106,16 @@
        */
       getMovies(){
         const moviesUrl = this.store.apiUrl + this.store.endPoint.movies;
-        if(this.paramsMovies.query === ''){console.log("LA QUERY E' VUOTA" );return;}
+        if(this.paramsMovies.query === ''){
+          console.log("LA QUERY E' VUOTA" );
+          return;
+        }
         axios.get(moviesUrl, {params: this.paramsMovies}).then((res)=>{
-        console.log(res.data.results);
-      }).catch((error)=>{
-        console.log(error);
-      })
+          this.store.moviesList=res.data.results;
+          console.log(this.store.moviesList);
+        }).catch((error)=>{
+          console.log(error);
+        });
       },
 
       /**
@@ -99,11 +125,15 @@
        */
       getSeries(){
         const seriesUrl = this.store.apiUrl + this.store.endPoint.series;
-        if(this.paramsSeries.query === ''){console.log("LA QUERY E' VUOTA" );return;}
-        return axios.get(seriesUrl, {params: this.paramsSeries}).then((res)=>{
-        console.log(res.data.results);
-      });
-      console.groupEnd();//DI get movies/series
+        if(this.paramsSeries.query === ''){
+          console.log("LA QUERY E' VUOTA" );
+          return;
+        }
+        axios.get(seriesUrl, {params: this.paramsSeries}).then((res)=>{          
+          this.store.seriesList=res.data.results;
+          console.log(this.store.seriesList);
+        });
+        console.groupEnd();//DI get movies/series
       }
 
     },
---
+++ src/components/CardComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="my-card debug">
+        <h4>{{title}}</h4>
+        <h4>{{originalTitle}}</h4>
+        <h4>{{originalLinguage}}</h4>
+        <h4>{{voteAverage}}</h4>
+    </div>
+    
+</template>
+
+<script>
+    export default{
+        name:'CardComponent',
+        props:{
+            title:String,
+            originalTitle:String,
+            originalLinguage:String,
+            voteAverage:Number,
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
